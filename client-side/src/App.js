b2a7f9a683852427fc7b9d8c7f34ebaeaca20b5a
venulgs <venu.koratla@labyrinthglobalsolutions.com>
2024-02-07 11:47:01 +0530

    changes done

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import AboutUs from "./Components/AboutUs/AboutUs";
+import HomePage from "./Components/HomePage";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/" element={<HomePage />} />
       </Routes>
     </>
   );
