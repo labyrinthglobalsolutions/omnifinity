50215340631de7bb8e156c5c42e15aa9a9963fd1
s umar <mdumarlgs122@gmail.com>
2024-02-08 10:34:41 +0530

    added admin component

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -4,6 +4,9 @@ import "./App.css";
 import AboutUs from "./Components/AboutUs/AboutUs";
 import HomePage from "./Components/HomePage/Home";
 import Navbar from "./Components/Header/Navbar";
+import AdminRegistration from "./AdminComponents/adminSignUp";
+import AdminEmailVerify from "./AdminComponents/adminEmailVerify";
+import AdminLogin from "./AdminComponents/adminLogin";
 
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
         <Route path="/about-us" element={<AboutUs />} />
 
         <Route exact path="/" Component={HomePage} />
+        <Route path="/adminregister" element={<AdminRegistration />} />
+        <Route path="/adminVerifyEmail" element={<AdminEmailVerify />} />
+        <Route path="/adminLogin" element={<AdminLogin />} />
       </Routes>
     </>
   );
