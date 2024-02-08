154b285ea2a76a98b4b0f02b3370d61ca2249e49
raviteja <ravitejasamboju@gmail.com>
2024-02-08 15:17:32 +0530

    contactusadded

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -7,17 +7,18 @@ import Navbar from "./Components/Header/Navbar";
 import AdminRegistration from "./AdminComponents/adminSignUp";
 import AdminEmailVerify from "./AdminComponents/adminEmailVerify";
 import AdminLogin from "./AdminComponents/adminLogin";
-
+import Contact from "./Components/ContactUs/Contact";
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
         <Route path="/about-us" element={<AboutUs />} />
-        <Route exact path="/" Component={HomePage} />
+        <Route exact path="/" element={<HomePage />} />
         <Route path="/adminregister" element={<AdminRegistration />} />
         <Route path="/adminVerifyEmail" element={<AdminEmailVerify />} />
         <Route path="/adminLogin" element={<AdminLogin />} />
+        <Route exact path="/contactUs" element={<Contact />} />
       </Routes>
     </>
   );
