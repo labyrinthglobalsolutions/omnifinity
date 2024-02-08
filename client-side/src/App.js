2d395421e961614f16a93ecd2b8a578178bafe59
raviteja <ravitejasamboju@gmail.com>
2024-02-08 15:36:32 +0530

    corrected total app.js and double home.js removed

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,55 +1,14 @@
-// import { Route, Routes } from "react-router-dom";
-
-
-import "./App.css";
-import AboutUs from "./Components/AboutUs/AboutUs";
-import HomePage from "./Components/HomePage/Home";
-import Navbar from "./Components/Header/Navbar";
-import AdminRegistration from "./AdminComponents/adminSignUp";
-import AdminEmailVerify from "./AdminComponents/adminEmailVerify";
-import AdminLogin from "./AdminComponents/adminLogin";
-import Contact from "./Components/ContactUs/Contact";
-
-// import "./App.css";
-// import AboutUs from "./Components/AboutUs/AboutUs";
-// import HomePage from "./Components/HomePage/Home";
-// import Navbar from "./Components/Header/Navbar";
-// import AdminRegistration from "./AdminComponents/adminSignUp";
-// import AdminEmailVerify from "./AdminComponents/adminEmailVerify";
-// import AdminLogin from "./AdminComponents/adminLogin";
-// import AllBanners from "./AdminComponents/allBanners";
-// import AdminPannel from "./AdminComponents/adminPanel";
-
-// function App() {
-//   return (
-//     <>
-//       <Navbar />
-//       <Routes>
-//         <Route path="/about-us" element={<AboutUs />} />
-//         <Route exact path="/" Component={HomePage} />
-//         <Route path="/adminRegister" element={<AdminRegistration />} />
-//         <Route path="/adminVerifyEmail" element={<AdminEmailVerify />} />
-//         <Route path="/adminLogin" element={<AdminLogin />} />
-//     <Route path="/adminDashboard" element={<AdminPannel />} />
-//       </Routes>
-//     </>
-//   );
-// }
-
-// export default App;
+import React from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
-
-import './App.css';
 import AboutUs from './Components/AboutUs/AboutUs';
 import HomePage from './Components/HomePage/Home';
 import Navbar from './Components/Header/Navbar';
 import AdminRegistration from './AdminComponents/adminSignUp';
 import AdminEmailVerify from './AdminComponents/adminEmailVerify';
 import AdminLogin from './AdminComponents/adminLogin';
-import AllBanners from './AdminComponents/allBanners';
 import AdminPannel from './AdminComponents/adminPanel';
+import Contact from './Components/ContactUs/Contact';
 import Footer from './Components/Footer/Footer';
-
 
 function App() {
   const location = useLocation();
@@ -63,18 +22,13 @@ function App() {
       <Routes>
         <Route path="/about-us" element={<AboutUs />} />
         <Route exact path="/" element={<HomePage />} />
-
         <Route path="/adminregister" element={<AdminRegistration />} />
         <Route path="/adminVerifyEmail" element={<AdminEmailVerify />} />
         <Route path="/adminLogin" element={<AdminLogin />} />
-        <Route exact path="/contactUs" element={<Contact />} / >
-        
-        <Route path="/adminRegister" element={<AdminRegistration />} />
-        <Route path="/adminVerifyEmail" element={<AdminEmailVerify />} />
-        <Route path="/adminLogin" element={<AdminLogin />} />
+        <Route path="/contactUs" element={<Contact />} />
         <Route path="/adminDashboard/*" element={<AdminPannel />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </>
   );
 }
